82a81b5638bd6648daba631ea58395136279e004
Vivek Kumar Singh <vivekkumarsingh.8991@gmail.com>
2019-08-30 12:20:00 +0530

    Initial Commit
    Created a Splash Screen and a Main Activity.

---
+++ app/src/main/java/com/vivek/agricultureahead/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.vivek.agricultureahead;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //Setting timer of splash screen
+    long Delay = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+        final ProgressBar circularProgressBar = (ProgressBar) findViewById(R.id.circularProgressBar);
+
+                // Creating a timer
+        Timer RunSplash = new Timer();
+
+        // Task to do when the timer ends
+        TimerTask ShowSplash = new TimerTask() {
+            @Override
+            public void run() {
+                // Close SplashScreenActivity.class
+                circularProgressBar.setVisibility(View.VISIBLE);
+                finish();
+
+                // Start MainActivity.class
+                Intent myIntent = new Intent(SplashScreen.this,
+                        MainActivity.class);
+                startActivity(myIntent);
+            }
+        };
+
+        // Start the timer
+        RunSplash.schedule(ShowSplash, Delay);
+
+    }
+}
