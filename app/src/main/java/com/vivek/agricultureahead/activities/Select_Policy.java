eaeaeb3914a3de54576a21734c49a208f3c3c8b7
Vivek Kumar Singh <vivekkumarsingh.8991@gmail.com>
2019-08-30 21:19:48 +0530

    Seventh Commit
    Created and Updated Government Schemes Activity as Select Policy Class

--- app/src/main/java/com/vivek/agricultureahead/activities/Select_Policy.java
+++ app/src/main/java/com/vivek/agricultureahead/activities/Select_Policy.java
@@ -1,21 +1,53 @@
 package com.vivek.agricultureahead.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.vivek.agricultureahead.R;
+import com.vivek.agricultureahead.adapters.PolicyAdapter;
 
 import java.util.ArrayList;
 
 
 public class Select_Policy extends AppCompatActivity {
 
+    private PolicyAdapter adapter;
+    private ArrayList list;
+    private RecyclerView recyclerView;
+
+    private String[] links;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        links=getResources().getStringArray(R.array.policies_link);
+
+        list = new ArrayList<>();
+
+        list=new ArrayList();
+
+        String[] array=getResources().getStringArray(R.array.policies);
+
+        for(int i=0;i<array.length;i++){
+            list.add(array[i]);
+        }
+
+        adapter = new PolicyAdapter(this,list,links);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        findViewById(R.id.progress).setVisibility(View.GONE);
     }
 }
---
+++ app/src/main/java/com/vivek/agricultureahead/activities/WebActivity.java
@@ -0,0 +1,61 @@
+package com.vivek.agricultureahead.activities;
+
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vivek.agricultureahead.R;
+
+
+public class WebActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        String link=getIntent().getStringExtra("link");
+        String title_temp=getIntent().getStringExtra("title");
+
+        if(title_temp!=null){
+            getSupportActionBar().setTitle(title_temp);
+        }
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= 11 && Build.VERSION.SDK_INT < 19) {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        webView.loadUrl(link);
+        webView.requestFocus();
+
+        progressDialog = new ProgressDialog(WebActivity.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                try {
+                    progressDialog.dismiss();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
